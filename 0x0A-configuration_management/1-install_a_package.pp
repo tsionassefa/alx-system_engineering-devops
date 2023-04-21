d3615a74c694e09136dd869f1dc9441eaf1e760a
tsionassefa <tsionassefa101@gmail.com>
2023-04-21 14:43:37 +0300

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# using puppet install flask from pip3
+#using pupet install flask
 package { 'flask':
-  ensure    => '2.1.0', 
-  provider  => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
