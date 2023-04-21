a8318cf4de726e0a075e5159eade67497731a090
tsionassefa <tsionassefa101@gmail.com>
2023-04-21 14:22:03 +0300

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create file at /tmp
 file { '/tmp/school':
-    ensure => 'file',
-    mode   => '0744',
-    owner  => www-data,
-    group  => www-data,
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',
- }
+}
