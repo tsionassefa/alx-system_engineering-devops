ae62ba9a896af06cff5190608f56bc666ce7d893
tsionassefa <tsionassefa101@gmail.com>
2023-05-22 12:10:39 +0300

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """script using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
-import requests as p
+import requests as r
 import sys
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = p.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = p.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 #    print(to_do)
     completed = [title.get("title") for title in to_do if
                  title.get('completed') is True]
